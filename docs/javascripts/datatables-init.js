9e37841448aa4c67ec90fd173dcf198c2b7b8c53
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-12-02 14:30:53 +0100

    Fix table ID in types.md from type-table to charts-table

--- docs/javascripts/datatables-init.js
+++ docs/javascripts/datatables-init.js
@@ -38,7 +38,36 @@ document.addEventListener('DOMContentLoaded', function () {
                     viewTotal: true
                 }
             }
-        }
+        },
+
+        initComplete: function () {
+            this.api()
+            .columns()
+            .every(function () {
+                let column = this;
+ 
+                // Create select element
+                let select = document.createElement('select');
+                select.add(new Option(''));
+                column.footer().replaceChildren(select);
+ 
+                // Apply listener for user change in value
+                select.addEventListener('change', function () {
+                    column
+                        .search(select.value, {exact: true})
+                        .draw();
+                });
+ 
+                // Add list of options
+                column
+                    .data()
+                    .unique()
+                    .sort()
+                    .each(function (d, j) {
+                        select.add(new Option(d));
+                    });
+            });
+        },
     });
 
     const $tableType = $('#type-table');   // ID‑basiert – bei Markdown‑Tabellen ggf. anpassen
